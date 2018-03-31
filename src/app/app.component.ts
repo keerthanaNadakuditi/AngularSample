4a3e0a8f3a453de506f367027aed5ac515a8a30b
Keerthana <n.keethana2989@gmail.com>
2018-03-31 00:58:24 -0500

    UI form logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'helloooo ';
+  inputNumber1: number;
+  inputNumber2: number;
+
+
+  @ViewChild('el1') el1: ElementRef;
+  @ViewChild('el2') el2: ElementRef;
+
+  submit() {
+    alert(Number(this.inputNumber1) + Number(this.inputNumber2));
+  }
+
+  checkInput1() {
+    if (+this.el1.nativeElement.value < 0) {
+      this.el1.nativeElement.className = 'error';
+    }
+    else {
+      this.el1.nativeElement.className = '';
+    }
+  }
+
+  checkInput2() {
+    if (+this.el2.nativeElement.value < 0) {
+      this.el2.nativeElement.className = 'error';
+    }
+    else {
+      this.el2.nativeElement.className = '';
+    }
+  }
+
+  disable() {
+    if (this.inputNumber1 < 0 || !this.inputNumber1 || this.inputNumber2 < 0 || !this.inputNumber2)
+      return true;
+    else
+      return false;
+  }
 }
