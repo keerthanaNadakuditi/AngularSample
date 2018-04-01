d126e5d7e6490af0193f30d32789d57148f103b5
Keerthana <n.keethana2989@gmail.com>
2018-04-01 18:38:54 -0500

    Test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,71 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { isNumber } from 'util';
+import { By } from '@angular/platform-browser';
+
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        FormsModule,
+        HttpClientModule
+      ]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+
+  it('Sample title test case', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(app.title).toEqual('EMC Task ');
   }));
-  it(`should have as title 'app'`, async(() => {
+
+  it('Any value -1 or less should cause the button to be disabled', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    // fixture.detectChanges();
+    // const compiled = fixture.debugElement.nativeElement;
+    const app = fixture.debugElement.componentInstance;
+    app.inputNumber1 = 10;
+    app.inputNumber2 = 5;
+    expect(app).toEqual('EMC Task ');
+    //set first input to < 0
+    //expect button to be disabled
+    let input  = fixture.debugElement.query(By.css('input[type=file]')).nativeElement;
+    input.value = {name: '0'};
+    input.dispatchEvent(new Event('input'));
+
+    // check that the event handler has been called
+    spyOn(app, 'fileChange');
+
+    expect(app.fileChange).toHaveBeenCalled();
+}));
+    //expect(compiled.querySelector('input').textContent).toEqual("");
+
+  // it('Any value -1 or less should cause the button to be disabled', async(() => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const app = fixture.debugElement.componentInstance;
+  //   // const compiled = fixture.debugElement.nativeElement();
+  //   expect(app.nativeElement.querySelector('input').textContent).toContain(Number);
+  //   // const app = fixture.debugElement.componentInstance;
+  //   // expect(app.title).toEqual('EMC Task ');
+  //   // console.log(temp);//.toEqual(undefined);
+  // }));
+
+  it('Value => 0 should be considered a valid input', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    //expect(app.checkInput1()).toEqual(Number);
   }));
-  it('should render title in a h1 tag', async(() => {
+  it('Any text values should cause the field to be disabled', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    const app = fixture.debugElement.componentInstance;
+    //expect(app.checkInput1()).toEqual(false);
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,59 +6,55 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'helloooo ';
+  title;
   inputNumber1: number;
   inputNumber2: number;
-
-  @ViewChild('el1') el1: ElementRef;
-  @ViewChild('el2') el2: ElementRef;
-
+  result: number;
 
   constructor(private http: HttpClient) { 
-
-  }
-
-
+    this.title = 'EMC Task ';
+    this.inputNumber1 = 0;
+    this.inputNumber2 = 0;
+   }
 
   ngOnInit() { 
+    this.getInputdata();
+  }
+
+  getInputdata = function getInput() {
     this.http.get('http://localhost:3000/getData')
       .subscribe(data => {
         let result: any = data;
         this.inputNumber1 = result.input1;
         this.inputNumber2 = result.input2;
-      });
+        //this.result = Number(this.inputNumber1) * Number(this.inputNumber2);
+      });;
   }
 
-  calculate() {
-    alert(Number(this.inputNumber1) + Number(this.inputNumber2));
+  calc = function calculate() {
+    this.result = Number(this.inputNumber1) * Number(this.inputNumber2);
+    this.saveInputData();
+  }
 
+  saveInputData = function saveData() {
     this.http.post('http://localhost:3000/saveData', { "input1": this.inputNumber1, "input2": this.inputNumber2 })
       .subscribe(data => { 
         console.log(data);
       });
-
-
   }
 
-  checkInput1() {
-    if (+this.el1.nativeElement.value < 0) {
-      this.el1.nativeElement.className = 'error';
+  check = function checkInput(ctrl) {
+    this.inputNumber = ctrl.currentTarget.value;
+
+    if (+ctrl.currentTarget.value < 0) {
+      ctrl.currentTarget.className = 'error';
     }
     else {
-      this.el1.nativeElement.className = '';
+      ctrl.currentTarget.className = '';
     }
   }
 
-  checkInput2() {
-    if (+this.el2.nativeElement.value < 0) {
-      this.el2.nativeElement.className = 'error';
-    }
-    else {
-      this.el2.nativeElement.className = '';
-    }
-  }
-
-  disable() {
+  buttonDisable = function disable() {
     if (this.inputNumber1 < 0 || this.inputNumber1 == undefined || this.inputNumber2 < 0 || this.inputNumber2 == undefined)
       return true;
     else
